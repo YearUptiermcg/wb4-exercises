cf0437fe7b8440670174b974e406ab12571e5876
Student <tierney@rcc.team>
2024-10-23 10:28:10 -0400

    added hotel class

--- wb4demos/src/main/java/com/pluralsight/Main.java
+++ wb4demos/src/main/java/com/pluralsight/Main.java
@@ -3,5 +3,13 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Car c = new Car("Jeep","CJ-7");
+        c.accelerate(5);
+        c.accelerate(10);
+        c.brake(3);
+        System.out.println(c.getSpeed());
+
     }
 }
