3adf9849dd8058d343118b2304bbb639af3f461a
Student <tierney@rcc.team>
2024-10-25 09:33:52 -0400

    classes added

--- Car-Dealershi-Objects/src/main/java/com/pluralsight/Main.java
+++ Car-Dealershi-Objects/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,15 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        UserInterface ui = new UserInterface();
+
+
     }
-}
+
